d40c7ce08f1186173ee96c63f67850a00e13931c
abhinavjnambiar <abhinav.jnambiar@gmail.com>
2025-05-19 12:44:18 +0530

    admin and user folder structure in vue js

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum','admin'])->group(function(){
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::get('/user', function (Request $request) {
-    return $request->user();
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-    Route::post('/logout',[AuthController::class,'logout']);
-
-});
-
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/login', [AuthController::class, 'login']);
